de6a5d95083a8561f8d569ba1d9bee34f21e6587
Apipol Niyomsak <makemek101@hotmail.com>
2017-04-19 17:33:41 +0700

    create bin/ ramdisk

--- build/docker-entrypoint.sh
+++ build/docker-entrypoint.sh
@@ -1,5 +1,10 @@
-#!/bin/bash -e
+#!/bin/bash
+
+set -e
 
 service xvfb start
+if [ "$RAMDISK" ]; then
+  cp -r $DEPLOY_DIR/bin $RAMDISK
+fi
 pm2-docker start process.yml --auto-exit --env $1
 exec "$@"
